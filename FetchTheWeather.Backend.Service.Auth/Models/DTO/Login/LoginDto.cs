37eb2ba694428fa86a3279f299e84b2cc858faf9
Pieter <pieter@devpieter.nl>
2025-12-08 15:32:24 +0100

    Implement authentication endpoints and update configuration

---
+++ FetchTheWeather.Backend.Service.Auth/Models/DTO/Login/LoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FetchTheWeather.Backend.Service.Auth.Models.DTO.Login;
+
+public class LoginDto
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
